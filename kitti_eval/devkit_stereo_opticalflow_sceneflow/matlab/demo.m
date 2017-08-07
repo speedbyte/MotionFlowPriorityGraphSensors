9462b212c67ce53200d1514f939b9ae9fae66490
Veikas <veikas@mangalyaan>
2017-08-07 19:07:11 +0200

    moved kitti eval

--- kitti_eval/devkit_stereo_opticalflow_sceneflow/matlab/demo.m
+++ kitti_eval/devkit_stereo_opticalflow_sceneflow/matlab/demo.m
@@ -5,7 +5,7 @@ clear all; close all; dbstop error;
 tau = [3 0.05];
 
 % original image
-orig = imread('../../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/image_2/000169_10.png');
+orig = imread('../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/image_2/000169_10.png');
 figure;
 imshow(orig);
 title('Original Image');
@@ -13,13 +13,13 @@ title('Original Image');
 % flow demo
 disp('Load and show optical flow field ... ');
 %F_est = flow_read('data/flow_est.png');
-%F_est = flow_read('../../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/flow_noc/000169_10.png');
+%F_est = flow_read('../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/flow_noc/000169_10.png');
 
 %F_gt  = flow_read('data/flow_gt.png');
-F_gt  = flow_read('../../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/flow_noc/000169_10.png');
+F_gt  = flow_read('../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/flow_noc/000169_10.png');
 
 %% DENSE
-F_est_dense = flow_read('../../../../project/BA_marcel/matlab/results_dense/000169_10.png');
+F_est_dense = flow_read('../../../project/BA_marcel/matlab/results_dense/000169_10.png');
 f_err = flow_error(F_gt,F_est_dense,tau);
 F_err = flow_error_image(F_gt,F_est_dense,tau);
 figure;
@@ -28,7 +28,7 @@ title(sprintf('Dense Flow Error: %.2f %',f_err*100));
 %%
 
 %% SPARSE
-F_est_sparse = flow_read('../../../../project/BA_marcel/matlab/results_sparse/000169_10.png');
+F_est_sparse = flow_read('../../../project/BA_marcel/matlab/results_sparse/000169_10.png');
 f_err = flow_error(F_gt,F_est_sparse,tau);
 F_err = flow_error_image(F_gt,F_est_sparse,tau);
 figure;
@@ -40,9 +40,9 @@ title(sprintf('Sparse Flow Error: %.2f %',f_err*100));
 %% Disparity 
 %disp('Load and show disparity map ... ');
 %%D_est = disp_read('data/disp_est.png');
-%D_est = disp_read('../../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/disp_occ_0/000169_10.png');
+%D_est = disp_read('../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/disp_occ_0/000169_10.png');
 %%D_gt  = disp_read('data/disp_gt.png');
-%D_gt  = disp_read('../../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/disp_occ_0/000169_10.png');
+%D_gt  = disp_read('../../../kitti_dataset/stereo_opticalflow_sceneflow_dataset_with_calib/training/disp_occ_0/000169_10.png');
 %d_err = disp_error(D_gt,D_est,tau);
 %D_err = disp_error_image(D_gt,D_est,tau);
 %figure;
