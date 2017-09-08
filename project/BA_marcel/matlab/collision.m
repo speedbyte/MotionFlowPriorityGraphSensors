fb12fbe71706ab944171a0fade5b17220d541df5
Marcel Frueh <fruehm@zamasu.Informatik.Uni-Tuebingen.De>
2017-09-08 16:43:28 +0200

    Code refactored; Static Ground Truth; Improved Plots

--- project/BA_marcel/matlab/collision.m
+++ project/BA_marcel/matlab/collision.m
@@ -1,5 +1,5 @@
 %This generates two objects moving somewhere in the plane, frame by
-%frame(at the moment not with the infinity path). 
+%frame(at the moment not with the infinity path).
 %Relative and absolute Ground Truth is generated
 %Relative and absolute Optical Flow is estimated.
 
@@ -9,148 +9,100 @@
 %%
 close all;
 
+%Initialisation of the flow object
 opticFlow=opticalFlowFarneback%('NoiseThreshold',0.04);
+%opticFlow=opticalFlowLK%('NoiseThreshold',0.04);
 
-img1 = zeros(375,1242,3,'uint8');
-img2 = zeros(375,1242,3,'uint8');
+%create frame matrix
+frame = zeros(375,1242,3,'uint8');
+%create absolute Flow matrix
 absoluteFlow = zeros(375,1242,3,'uint16');
 
-xMovement = 4;
+
+%Create the two objects(corresponding to pedesterians)
+%and their corresponding movement in x and y direction.
+xMovement =4;
 yMovement = 1;
 
-width = 320:380;
-height = 46:106;
+width = 320:350;
+height = 26:106;
 
-secondObjectWidth = 520:580;
-secondObjectHeight = 5:65;
+secondObjectWidth = 520:550;
+secondObjectHeight = 100:180;
 
 secondXMovement = -5;
 secondYMovement = 1;
 
+%how many iterations?
+maxIteration = 20;
 %%
-%%Initialize
-%%white background
-for k=1:375
-    for j=1:1242
-        for i=1:3
-            img1(k,j,i) = 255;
-            img2(k,j,i) = 255;
-            
-        end
-    end
-end
-
- 
-%%create frame
-for k = height
-    for j= width
-        for i=1:3
-            img1(k,j,i)= 0;
-            if mod(k,25) == 0
-                img1(k,j,i) = 255;
-            end
-            if mod(j,25) == 0
-                img1(k,j,i) = 255;
-            end
-        end
-    end
-end
-
-
-%%expand with 2nd object
-for k = secondObjectHeight
-    for j= secondObjectWidth
-        for i=1:3
-            img1(k,j,i)= 0;
-            if mod(k,5) == 0
-                img1(k,j,i) = 255;
-            end
-            if mod(j,5) == 0
-                img1(k,j,i) = 255;
-            end
-        end
-    end
-end
-figure('Name', 'Start');
-imshow(img1);
-pause(1);
-close;
-%%
-
-
 %%move the objects
-for counter=1:20
-
-  img1 = Movement(height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement);
-
-%%
-
-
-
-figure('Name', 'First Frame');
-imshow(img1);
-
-pause(0.5);
-close all;
-%%
-%calculate absolute Ground Truth and create Kitti png for relative Ground Truth%
-absoluteGT = gT(height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement);
-
-
-%%OpticalFlow
-img1_gray = rgb2gray(img1);
-flow_img1 = estimateFlow(opticFlow,img1_gray);
-
-%%get absolute estimated flow. At the moment hacked, because I only watch
-%%for entries, where the objects are. If the flow detects movement beyond the object, then
-%%it is capped in the absolute flow. 
-
-for k=height
-    for j=width
-        absoluteFlow(k,j,1) = flow_img1.Vx(k,j)+j;
-        absoluteFlow(k,j,2) = flow_img1.Vy(k,j)+k;
-        absoluteFlow(k,j,3) = 1;
-    end
-end
-
-for k=secondObjectHeight
-    for j=secondObjectWidth
-        absoluteFlow(k,j,1) = flow_img1.Vx(k,j)+j;
-        absoluteFlow(k,j,2) = flow_img1.Vy(k,j)+k;
-        absoluteFlow(k,j,3) = 1;
-    end
-end
-
-
-%% This shows the Optical Flow with arrows
-figure('Name', 'Displacement Vector');
-imshow(img1);
-hold on
-plot(flow_img1,'DecimationFactor',[6 6],'ScaleFactor',13);
-hold off
-
-
-%% collision checkers
-gtCol = gtCollision( absoluteGT, width,height,xMovement,yMovement, secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement );
-
-flow(:,:,1) = flow_img1.Vx;
-flow(:,:,2) = flow_img1.Vy;
-
-estCol = flowCollision( absoluteFlow,flow, width,height, secondObjectWidth,secondObjectHeight);
-
-if gtCol == 1
-    disp('GT: Collision in the future');
-end
-if estCol == 1
-    disp('FLOW: Collision in the future');
-end
-%%
+for counter=1:maxIteration
+    %Move the frames
+    frame = Movement(height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement);
+    %calculate absolute Ground Truth
+    absoluteGroundTruth = gT(height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement,'absolute');
     
-%adjust object position according to movement
-width=width+xMovement;
-height=height+yMovement;
-secondObjectHeight=secondObjectHeight+secondYMovement;
-secondObjectWidth=secondObjectWidth+secondXMovement;
-
-
+    if counter == 1
+        figure(2),
+        imshow(frame)
+        pause(2)
+        close;
+    end
+    
+    %%OpticalFlow
+    frame_gray = rgb2gray(frame);
+    flow_frame = estimateFlow(opticFlow,frame_gray);
+    
+    if counter > 1 %to kick out bad first flow
+        
+        %get absolute estimated flow.
+        for k=height
+            for j=width
+                absoluteFlow(k,j,1) = flow_frame.Vx(k,j)+j;
+                absoluteFlow(k,j,2) = flow_frame.Vy(k,j)+k;
+                absoluteFlow(k,j,3) = 1;
+            end
+        end
+        for k=secondObjectHeight
+            for j=secondObjectWidth
+                absoluteFlow(k,j,1) = flow_frame.Vx(k,j)+j;
+                absoluteFlow(k,j,2) = flow_frame.Vy(k,j)+k;
+                absoluteFlow(k,j,3) = 1;
+            end
+        end
+        
+        
+        %% collision checkers
+        groundTruthCollision = gtCollision(maxIteration,absoluteGroundTruth, width,height,xMovement,yMovement, secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement,counter );
+        
+        %create flow matrix to store the estimated displacemend in.
+        flow(:,:,1) = flow_frame.Vx;
+        flow(:,:,2) = flow_frame.Vy;
+        
+        %estimate if a collision is about to happen.
+        estimatedCollision = flowCollision( absoluteFlow,flow, width,height, secondObjectWidth,secondObjectHeight);
+        
+        if groundTruthCollision == 1
+            disp('Ground Truth: The objects will collide.');
+        %    pause(3);
+        end
+        
+        if estimatedCollision == 1
+            disp('FLOW: Collision in the future');
+        end
+        
+        %plot everything
+        plotter(frame,flow_frame);
+    end
+    %adjust object position according to movement
+    width=width+xMovement;
+    height=height+yMovement;
+    secondObjectHeight=secondObjectHeight+secondYMovement;
+    secondObjectWidth=secondObjectWidth+secondXMovement;
+    
+    
+    
+    
+    
 end
--- project/BA_marcel/matlab/gT.m
+++ project/BA_marcel/matlab/gT.m
@@ -1,46 +1,44 @@
-function [img4] = gT( height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement )
+function [response] = gT( height,width,xMovement,yMovement,secondObjectWidth,secondObjectHeight,secondXMovement,secondYMovement,parameter )
 %%generate GT
 
-img3 = zeros(375,1242,3,'uint16');
-img4 = zeros(375,1242,3);
+relativeGroundTruth = zeros(375,1242,3,'uint16');
+absoluteGroundTruth = zeros(375,1242,3);
 
 for k=height
     for j=width
-        img3(k,j,1) = xMovement;
-        img3(k,j,2) = yMovement;
-        img3(k,j,3) = 1;
-        img4(k,j,1) = xMovement+j;
-        img4(k,j,2) = yMovement+k;
-        img4(k,j,3) = 1;
+        relativeGroundTruth(k,j,1) = xMovement;
+        relativeGroundTruth(k,j,2) = yMovement;
+        relativeGroundTruth(k,j,3) = 1;
+        absoluteGroundTruth(k,j,1) = xMovement+j;
+        absoluteGroundTruth(k,j,2) = yMovement+k;
+        absoluteGroundTruth(k,j,3) = 1;
     end
 end
 
 for k=secondObjectHeight
     for j=secondObjectWidth
-        img3(k,j,1) = secondXMovement;
-        img3(k,j,2) = secondYMovement;
-        img3(k,j,3) = 1;
-        img4(k,j,1) = secondXMovement+j;
-        img4(k,j,2) = secondYMovement+k;
-        img4(k,j,3) = 1;
+        relativeGroundTruth(k,j,1) = secondXMovement;
+        relativeGroundTruth(k,j,2) = secondYMovement;
+        relativeGroundTruth(k,j,3) = 1;
+        absoluteGroundTruth(k,j,1) = secondXMovement+j;
+        absoluteGroundTruth(k,j,2) = secondYMovement+k;
+        absoluteGroundTruth(k,j,3) = 1;
     end
 end
 
-%%Quantization
-gtX = (img3(:,:,1)*64)+2^15;
-gtY = (img3(:,:,2)*64)+2^15;
-
 %Create png Matrix with 3 channels: OF in vertical. OF in Horizontal.
 %and Validation bit
-res = cat(3,img3(:,:,1),img3(:,:,2),img3(:,:,3));
-re =  uint16(res);
-%disp(re(:,:,1));
+kittiGT = cat(3,relativeGroundTruth(:,:,1),relativeGroundTruth(:,:,2),relativeGroundTruth(:,:,3));
+kittiGT =  uint16(kittiGT);
 
-%Creates OF png for Kitti. Not very lucidly if looked at
+if strcmp(parameter,'absolute')
+    response = absoluteGroundTruth;
+elseif strcmp(parameter,'relative')
+    response = relativeGroundTruth;
+elseif strcmp(parameter,'kitti')
+    response = kittiGT;
+end
 
-%Comment in if needed
-%imwrite( re,'GroundTruth.png');
-%disp('GroundTruth has been generated')
 
 end
 
