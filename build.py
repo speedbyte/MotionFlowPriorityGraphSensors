c81feb53996279eb675007dbe7bdafb87f6b7424
veikas@jcbose <vagrawal@hs-esslingen.de>
2017-10-07 20:23:23 +0200

    adding generic compiliation process for every module independant of modules

--- build.py
+++ build.py
@@ -10,11 +10,10 @@ import sys
 
 
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
-OPENCV_PWD = SOURCE_DIR + "/libs/opencv"
-OPENCV_INSTALL_PWD = SOURCE_DIR + "/libs/opencv-install"
 
 def call_shell_command(command):
     proc = subprocess.check_call(command.split(' '), stderr=subprocess.PIPE)
+    #apt-cache search ^libflann-dev | cut -d ' ' -f1
     #proc = subprocess.Popen(command.split(' '), stderr=subprocess.PIPE)
     #proc.wait()
     #out, err = proc.communicate()
@@ -28,37 +27,7 @@ def call_shell_command(command):
 
 
 def enter_opencv_fn(args):
-    os.chdir(SOURCE_DIR)
-    print args.OPENCV_OPTION
-    build_option = args.BUILD_OPTION
-    print build_option
-    os.chdir(OPENCV_PWD)
-    print "Building in " + os.getcwd()
-    if build_option == "clean":
-        print "cleaning opencv ....";
-        call_shell_command("rm -rf cmake-build-debug")
-        call_shell_command("rm -rf OPENCV_INSTALL_PWD/*")
-    elif build_option == "manual":
-        raw_input("Press enter to continue")
-    print "Configuring opencv"
-    if build_option == "manual":
-        raw_input("Press enter to continue")
-    call_shell_command("mkdir -p cmake-build-debug")
-    os.chdir("cmake-build-debug")
-    command = "cmake -Wno -dev -Wl,-rpath=/usr/local/lib -DENABLE_PRECOMPILED_HEADERS=OFF " \
-              "-DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=SOURCE_DIR/libs/opencv-install " \
-              "-DWITH_GTK=ON -DBUILD_JPEG=ON .."
-    call_shell_command(command)
-    if build_option == "manual":
-        raw_input("Press enter to continue")
-    command = "time make -j 8"
-    call_shell_command(command)
-    if build_option == "manual":
-        raw_input("Press enter to continue")
-    command = "time make install"
-    call_shell_command(command)
-    os.chdir(SOURCE_DIR)
-
+    pass
 
 def enter_pcl_fn(args):
     os.chdir('libs/pcl/cmake-build-debug')
@@ -78,13 +47,64 @@ def enter_dummy_fn(args):
 
 
 def parse_arguements(args):
-    if args.OPENCV_OPTION:
-        enter_opencv_fn(args)
-    if args.PCL_OPTION:
-        enter_pcl_fn(args)
+    list_of_options = list()
+    opencv_properties = ("opencv", 3.2)
+    pcl_properties = ("pcl", 1.8)
+    list_of_options.append(opencv_properties)
+    list_of_options.append(pcl_properties)
+
+    build_properties = (args.OPENCV_OPTION, args.PCL_OPTION)
+
+    zipped = zip(list_of_options, build_properties)
+    build_option = args.BUILD_OPTION
+    make_power = subprocess.check_output("getconf _NPROCESSORS_ONLN", shell=True)
+    make_power = make_power.strip('\n')
+    for count in range(len(zipped)):
+        if zipped[count][1]:
+            module = SOURCE_DIR + "/libs/" + zipped[count][0][0]
+            module_install = module + "-install"
+            print "starting building %s" % module
+            os.chdir(SOURCE_DIR)
+            os.chdir(module)
+            print "Building in " + os.getcwd()
+            if build_option == "clean":
+                print "cleaning opencv ....";
+                call_shell_command("rm -rf cmake-build-debug")
+                call_shell_command("rm -rf libs/" + module_install + "/*")
+            elif build_option == "manual":
+                raw_input("Press enter to continue")
+            print "Configuring %s" %(module)
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            call_shell_command("mkdir -p cmake-build-debug")
+            os.chdir("cmake-build-debug")
+            command = "cmake -Wno -dev -Wl,-rpath=/usr/local/lib -DENABLE_PRECOMPILED_HEADERS=OFF " \
+                      "-DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=" + module_install + " " + \
+                      "-DWITH_GTK=ON -DBUILD_JPEG=ON .."
+            print command
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            call_shell_command(command)
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            command = "time make -j " + make_power
+            print command
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            call_shell_command(command)
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            command = "time make install"
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            call_shell_command(command)
+            if build_option == "manual":
+                raw_input("Press enter to continue")
+            os.chdir(SOURCE_DIR)
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--test', action='store_true', help='test script')
 parser.add_argument('-o', '--opencv', action='store_true', dest='OPENCV_OPTION', help='builds opencv')
 parser.add_argument('-p', '--pcl', action='store_true', dest='PCL_OPTION', help='builds pcl')
 parser.add_argument('-t', dest="BUILD_OPTION", choices=('clean', 'manual'), help='build parameters')
