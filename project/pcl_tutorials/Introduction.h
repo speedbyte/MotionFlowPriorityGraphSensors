1ddae8eb456ad5c2e5509b9e25c3451be881c865
Vikas@ducky <no_pain_no_gain@no_email_id.com>
2017-10-08 17:30:45 +0200

    adding pcl demo pictures

--- project/pcl_tutorials/Introduction.h
+++ project/pcl_tutorials/Introduction.h
@@ -4,4 +4,140 @@
  PCL tutorials
  CloudViewer
  PCL Visualization
+
+-- looking for PCL_COMMON
+-- looking for PCL_OCTREE
+-- looking for PCL_IO
+-- looking for PCL_KDTREE
+-- looking for PCL_SEARCH
+-- looking for PCL_SAMPLE_CONSENSUS
+-- looking for PCL_FILTERS
+-- looking for PCL_2D
+-- looking for PCL_GEOMETRY
+-- looking for PCL_FEATURES
+-- looking for PCL_ML
+-- looking for PCL_SEGMENTATION
+-- looking for PCL_SURFACE
+-- looking for PCL_REGISTRATION
+-- looking for PCL_KEYPOINTS
+-- looking for PCL_TRACKING
+-- looking for PCL_RECOGNITION
+-- looking for PCL_STEREO
+-- Found PCL: /usr/local/lib/libboost_system.so
+/usr/local/lib/libboost_filesystem.so
+/usr/local/lib/libboost_thread.so
+/usr/local/lib/libboost_date_time.so
+/usr/local/lib/libboost_iostreams.so
+/usr/local/lib/libboost_serialization.so
+optimized;/usr/local/lib/libpcl_common.so
+debug;/usr/local/lib/libpcl_common.so
+optimized;/usr/local/lib/libpcl_octree.so
+debug;/usr/local/lib/libpcl_octree.so
+vtkIOExport;vtkCommonCore;vtksys;vtkFiltersGeometry;vtkFiltersCore;vtkCommonExecutionModel;vtkCommonDataModel;vtkCommonMath;vtkCommonMisc;vtkCommonSystem;vtkCommonTransforms;vtkIOImage;vtkDICOMParser;vtkIOCore;/usr/lib64/libz.so
+vtkmetaio;/usr/lib64/libjpeg.so
+/usr/lib64/libpng.so
+/usr/lib64/libtiff.so
+vtkImagingCore;vtkRenderingAnnotation;vtkFiltersSources;vtkCommonComputationalGeometry;vtkFiltersGeneral;vtkImagingColor;vtkRenderingFreeType;vtkRenderingCore;vtkFiltersExtraction;vtkFiltersStatistics;vtkImagingFourier;vtkalglib;/usr/lib64/libfreetype.so
+vtkftgl;vtkRenderingContext2D;vtkRenderingOpenGL;vtkImagingHybrid;vtkRenderingGL2PS;/usr/lib64/libgl2ps.so
+vtkRenderingLabel;vtkChartsCore;vtkCommonColor;vtkInfovisCore;vtkFiltersImaging;vtkImagingGeneral;vtkImagingSources;vtkIOGeometry;/usr/lib64/libjsoncpp.so
+vtkFiltersTexture;vtkFiltersModeling;vtkFiltersVerdict;verdict;vtkGUISupportQt;vtkInteractionStyle;vtkGUISupportQtOpenGL;vtkGUISupportQtSQL;vtkIOSQL;vtksqlite;vtkFiltersParallel;vtkParallelCore;vtkIOLegacy;vtkGUISupportQtWebkit;vtkViewsQt;vtkViewsInfovis;vtkInfovisLayout;vtkViewsCore;vtkInteractionWidgets;vtkFiltersHybrid;vtkRenderingVolume;vtkDomainsChemistry;vtkIOXML;vtkIOXMLParser;/usr/lib64/libexpat.so
+vtkFiltersParallelImaging;vtkIOImport;vtkFiltersAMR;vtkGeovisCore;vtkproj4;vtkIOAMR;/usr/lib64/libdl.so
+/usr/lib64/libm.so
+/usr/lib64/libhdf5_hl.so
+/usr/lib64/libhdf5.so
+vtkFiltersFlowPaths;vtkFiltersSelection;vtkFiltersGeneric;vtkIOEnSight;vtkIOExodus;vtkexoIIc;/usr/lib64/libnetcdf_c++.so
+/usr/lib64/libnetcdf.so
+vtkFiltersHyperTree;vtkFiltersProgrammable;vtkFiltersStatisticsGnuR;vtkIOInfovis;/usr/lib64/libxml2.so
+vtkFiltersSMP;vtkIOLSDyna;vtkRenderingFreeTypeOpenGL;vtkInteractionImage;vtkIOMINC;vtkIOMovie;/usr/lib64/libtheoraenc.so
+/usr/lib64/libtheoradec.so
+/usr/lib64/libogg.so
+vtkRenderingVolumeAMR;vtkIONetCDF;vtkIOPLY;vtkIOParallel;vtkTestingRendering;vtkIOVideo;vtkViewsContext2D;vtkRenderingImage;vtkRenderingLIC;vtkRenderingLOD;vtkImagingMath;vtkImagingMorphological;vtkLocalExample;vtkImagingStatistics;vtkImagingStencil;vtkViewsGeovis;vtkRenderingQt;/usr/lib64/libpython2.7.so
+vtkRenderingVolumeOpenGL;vtkWrappingJava;vtkWrappingTools;vtkWrappingPythonCore;optimized;/usr/local/lib/libpcl_io.so
+debug;/usr/local/lib/libpcl_io.so
+optimized;/usr/lib64/libflann_cpp.so
+debug;/usr/lib64/libflann_cpp.so
+optimized;/usr/local/lib/libpcl_kdtree.so
+debug;/usr/local/lib/libpcl_kdtree.so
+optimized;/usr/local/lib/libpcl_search.so
+debug;/usr/local/lib/libpcl_search.so
+optimized;/usr/local/lib/libpcl_sample_consensus.so
+debug;/usr/local/lib/libpcl_sample_consensus.so
+optimized;/usr/local/lib/libpcl_filters.so
+debug;/usr/local/lib/libpcl_filters.so
+optimized;/usr/local/lib/libpcl_features.so
+debug;/usr/local/lib/libpcl_features.so
+optimized;/usr/local/lib/libpcl_ml.so
+debug;/usr/local/lib/libpcl_ml.so
+optimized;/usr/local/lib/libpcl_segmentation.so
+debug;/usr/local/lib/libpcl_segmentation.so
+optimized;/usr/local/lib/libpcl_surface.so
+debug;/usr/local/lib/libpcl_surface.so
+optimized;/usr/local/lib/libpcl_registration.so
+debug;/usr/local/lib/libpcl_registration.so
+optimized;/usr/local/lib/libpcl_keypoints.so
+debug;/usr/local/lib/libpcl_keypoints.so
+optimized;/usr/local/lib/libpcl_tracking.so
+debug;/usr/local/lib/libpcl_tracking.so
+optimized;/usr/local/lib/libpcl_recognition.so
+debug;/usr/local/lib/libpcl_recognition.so
+optimized;/usr/local/lib/libpcl_stereo.so
+debug;/usr/local/lib/libpcl_stereo.so
+/usr/local/lib/libboost_system.so
+/usr/local/lib/libboost_filesystem.so
+/usr/local/lib/libboost_thread.so
+/usr/local/lib/libboost_date_time.so
+/usr/local/lib/libboost_iostreams.so
+/usr/local/lib/libboost_serialization.so
+optimized;/usr/lib64/libflann_cpp.so
+debug;/usr/lib64/libflann_cpp.so
+vtkIOExport;vtkCommonCore;vtksys;vtkFiltersGeometry;vtkFiltersCore;vtkCommonExecutionModel;vtkCommonDataModel;vtkCommonMath;vtkCommonMisc;vtkCommonSystem;vtkCommonTransforms;vtkIOImage;vtkDICOMParser;vtkIOCore;/usr/lib64/libz.so
+vtkmetaio;/usr/lib64/libjpeg.so
+/usr/lib64/libpng.so
+/usr/lib64/libtiff.so
+vtkImagingCore;vtkRenderingAnnotation;vtkFiltersSources;vtkCommonComputationalGeometry;vtkFiltersGeneral;vtkImagingColor;vtkRenderingFreeType;vtkRenderingCore;vtkFiltersExtraction;vtkFiltersStatistics;vtkImagingFourier;vtkalglib;/usr/lib64/libfreetype.so
+vtkftgl;vtkRenderingContext2D;vtkRenderingOpenGL;vtkImagingHybrid;vtkRenderingGL2PS;/usr/lib64/libgl2ps.so
+vtkRenderingLabel;vtkChartsCore;vtkCommonColor;vtkInfovisCore;vtkFiltersImaging;vtkImagingGeneral;vtkImagingSources;vtkIOGeometry;/usr/lib64/libjsoncpp.so
+vtkFiltersTexture;vtkFiltersModeling;vtkFiltersVerdict;verdict;vtkGUISupportQt;vtkInteractionStyle;vtkGUISupportQtOpenGL;vtkGUISupportQtSQL;vtkIOSQL;vtksqlite;vtkFiltersParallel;vtkParallelCore;vtkIOLegacy;vtkGUISupportQtWebkit;vtkViewsQt;vtkViewsInfovis;vtkInfovisLayout;vtkViewsCore;vtkInteractionWidgets;vtkFiltersHybrid;vtkRenderingVolume;vtkDomainsChemistry;vtkIOXML;vtkIOXMLParser;/usr/lib64/libexpat.so
+vtkFiltersParallelImaging;vtkIOImport;vtkFiltersAMR;vtkGeovisCore;vtkproj4;vtkIOAMR;/usr/lib64/libdl.so
+/usr/lib64/libm.so
+/usr/lib64/libhdf5_hl.so
+/usr/lib64/libhdf5.so
+vtkFiltersFlowPaths;vtkFiltersSelection;vtkFiltersGeneric;vtkIOEnSight;vtkIOExodus;vtkexoIIc;/usr/lib64/libnetcdf_c++.so
+/usr/lib64/libnetcdf.so
+vtkFiltersHyperTree;vtkFiltersProgrammable;vtkFiltersStatisticsGnuR;vtkIOInfovis;/usr/lib64/libxml2.so
+vtkFiltersSMP;vtkIOLSDyna;vtkRenderingFreeTypeOpenGL;vtkInteractionImage;vtkIOMINC;vtkIOMovie;/usr/lib64/libtheoraenc.so
+/usr/lib64/libtheoradec.so
+/usr/lib64/libogg.so
+vtkRenderingVolumeAMR;vtkIONetCDF;vtkIOPLY;vtkIOParallel;vtkTestingRendering;vtkIOVideo;vtkViewsContext2D;vtkRenderingImage;vtkRenderingLIC;vtkRenderingLOD;vtkImagingMath;vtkImagingMorphological;vtkLocalExample;vtkImagingStatistics;vtkImagingStencil;vtkViewsGeovis;vtkRenderingQt;/usr/lib64/libpython2.7.so
+vtkRenderingVolumeOpenGL;vtkWrappingJava;vtkWrappingTools;vtkWrappingPythonCore (Required is exact version "1.8.1.99")
+Libraries: /usr/local/lib/libboost_system.so
+/usr/local/lib/libboost_filesystem.so
+/usr/local/lib/libboost_thread.so
+/usr/local/lib/libboost_date_time.so
+/usr/local/lib/libboost_iostreams.so
+/usr/local/lib/libboost_serialization.sooptimized/usr/local/lib/libpcl_common.sodebug/usr/local/lib/libpcl_common.sooptimized/usr/local/lib/libpcl_octree.sodebug/usr/local/lib/libpcl_octree.sovtkIOExportvtkCommonCorevtksysvtkFiltersGeometryvtkFiltersCorevtkCommonExecutionModelvtkCommonDataModelvtkCommonMathvtkCommonMiscvtkCommonSystemvtkCommonTransformsvtkIOImagevtkDICOMParservtkIOCore/usr/lib64/libz.sovtkmetaio/usr/lib64/libjpeg.so
+/usr/lib64/libpng.so
+/usr/lib64/libtiff.sovtkImagingCorevtkRenderingAnnotationvtkFiltersSourcesvtkCommonComputationalGeometryvtkFiltersGeneralvtkImagingColorvtkRenderingFreeTypevtkRenderingCorevtkFiltersExtractionvtkFiltersStatisticsvtkImagingFouriervtkalglib/usr/lib64/libfreetype.sovtkftglvtkRenderingContext2DvtkRenderingOpenGLvtkImagingHybridvtkRenderingGL2PS/usr/lib64/libgl2ps.sovtkRenderingLabelvtkChartsCorevtkCommonColorvtkInfovisCorevtkFiltersImagingvtkImagingGeneralvtkImagingSourcesvtkIOGeometry/usr/lib64/libjsoncpp.sovtkFiltersTexturevtkFiltersModelingvtkFiltersVerdictverdictvtkGUISupportQtvtkInteractionStylevtkGUISupportQtOpenGLvtkGUISupportQtSQLvtkIOSQLvtksqlitevtkFiltersParallelvtkParallelCorevtkIOLegacyvtkGUISupportQtWebkitvtkViewsQtvtkViewsInfovisvtkInfovisLayoutvtkViewsCorevtkInteractionWidgetsvtkFiltersHybridvtkRenderingVolumevtkDomainsChemistryvtkIOXMLvtkIOXMLParser/usr/lib64/libexpat.sovtkFiltersParallelImagingvtkIOImportvtkFiltersAMRvtkGeovisCorevtkproj4vtkIOAMR/usr/lib64/libdl.so
+/usr/lib64/libm.so
+/usr/lib64/libhdf5_hl.so
+/usr/lib64/libhdf5.sovtkFiltersFlowPathsvtkFiltersSelectionvtkFiltersGenericvtkIOEnSightvtkIOExodusvtkexoIIc/usr/lib64/libnetcdf_c++.so
+/usr/lib64/libnetcdf.sovtkFiltersHyperTreevtkFiltersProgrammablevtkFiltersStatisticsGnuRvtkIOInfovis/usr/lib64/libxml2.sovtkFiltersSMPvtkIOLSDynavtkRenderingFreeTypeOpenGLvtkInteractionImagevtkIOMINCvtkIOMovie/usr/lib64/libtheoraenc.so
+/usr/lib64/libtheoradec.so
+/usr/lib64/libogg.sovtkRenderingVolumeAMRvtkIONetCDFvtkIOPLYvtkIOParallelvtkTestingRenderingvtkIOVideovtkViewsContext2DvtkRenderingImagevtkRenderingLICvtkRenderingLODvtkImagingMathvtkImagingMorphologicalvtkLocalExamplevtkImagingStatisticsvtkImagingStencilvtkViewsGeovisvtkRenderingQt/usr/lib64/libpython2.7.sovtkRenderingVolumeOpenGLvtkWrappingJavavtkWrappingToolsvtkWrappingPythonCoreoptimized/usr/local/lib/libpcl_io.sodebug/usr/local/lib/libpcl_io.sooptimized/usr/lib64/libflann_cpp.sodebug/usr/lib64/libflann_cpp.sooptimized/usr/local/lib/libpcl_kdtree.sodebug/usr/local/lib/libpcl_kdtree.sooptimized/usr/local/lib/libpcl_search.sodebug/usr/local/lib/libpcl_search.sooptimized/usr/local/lib/libpcl_sample_consensus.sodebug/usr/local/lib/libpcl_sample_consensus.sooptimized/usr/local/lib/libpcl_filters.sodebug/usr/local/lib/libpcl_filters.sooptimized/usr/local/lib/libpcl_features.sodebug/usr/local/lib/libpcl_features.sooptimized/usr/local/lib/libpcl_ml.sodebug/usr/local/lib/libpcl_ml.sooptimized/usr/local/lib/libpcl_segmentation.sodebug/usr/local/lib/libpcl_segmentation.sooptimized/usr/local/lib/libpcl_surface.sodebug/usr/local/lib/libpcl_surface.sooptimized/usr/local/lib/libpcl_registration.sodebug/usr/local/lib/libpcl_registration.sooptimized/usr/local/lib/libpcl_keypoints.sodebug/usr/local/lib/libpcl_keypoints.sooptimized/usr/local/lib/libpcl_tracking.sodebug/usr/local/lib/libpcl_tracking.sooptimized/usr/local/lib/libpcl_recognition.sodebug/usr/local/lib/libpcl_recognition.sooptimized/usr/local/lib/libpcl_stereo.sodebug/usr/local/lib/libpcl_stereo.so
+/usr/local/lib/libboost_system.so
+/usr/local/lib/libboost_filesystem.so
+/usr/local/lib/libboost_thread.so
+/usr/local/lib/libboost_date_time.so
+/usr/local/lib/libboost_iostreams.so
+/usr/local/lib/libboost_serialization.sooptimized/usr/lib64/libflann_cpp.sodebug/usr/lib64/libflann_cpp.sovtkIOExportvtkCommonCorevtksysvtkFiltersGeometryvtkFiltersCorevtkCommonExecutionModelvtkCommonDataModelvtkCommonMathvtkCommonMiscvtkCommonSystemvtkCommonTransformsvtkIOImagevtkDI
+        COMParservtkIOCore/usr/lib64/libz.sovtkmetaio/usr/lib64/libjpeg.so
+/usr/lib64/libpng.so
+/usr/lib64/libtiff.sovtkImagingCorevtkRenderingAnnotationvtkFiltersSourcesvtkCommonComputationalGeometryvtkFiltersGeneralvtkImagingColorvtkRenderingFreeTypevtkRenderingCorevtkFiltersExtractionvtkFiltersStatisticsvtkImagingFouriervtkalglib/usr/lib64/libfreetype.sovtkftglvtkRenderingContext2DvtkRenderingOpenGLvtkImagingHybridvtkRenderingGL2PS/usr/lib64/libgl2ps.sovtkRenderingLabelvtkChartsCorevtkCommonColorvtkInfovisCorevtkFiltersImagingvtkImagingGeneralvtkImagingSourcesvtkIOGeometry/usr/lib64/libjsoncpp.sovtkFiltersTexturevtkFiltersModelingvtkFiltersVerdictverdictvtkGUISupportQtvtkInteractionStylevtkGUISupportQtOpenGLvtkGUISupportQtSQLvtkIOSQLvtksqlitevtkFiltersParallelvtkParallelCorevtkIOLegacyvtkGUISupportQtWebkitvtkViewsQtvtkViewsInfovisvtkInfovisLayoutvtkViewsCorevtkInteractionWidgetsvtkFiltersHybridvtkRenderingVolumevtkDomainsChemistryvtkIOXMLvtkIOXMLParser/usr/lib64/libexpat.sovtkFiltersParallelImagingvtkIOImportvtkFiltersAMRvtkGeovisCorevtkproj4vtkIOAMR/usr/lib64/libdl.so
+/usr/lib64/libm.so
+/usr/lib64/libhdf5_hl.so
+/usr/lib64/libhdf5.sovtkFiltersFlowPathsvtkFiltersSelectionvtkFiltersGenericvtkIOEnSightvtkIOExodusvtkexoIIc/usr/lib64/libnetcdf_c++.so
+/usr/lib64/libnetcdf.sovtkFiltersHyperTreevtkFiltersProgrammablevtkFiltersStatisticsGnuRvtkIOInfovis/usr/lib64/libxml2.sovtkFiltersSMPvtkIOLSDynavtkRenderingFreeTypeOpenGLvtkInteractionImagevtkIOMINCvtkIOMovie/usr/lib64/libtheoraenc.so
+/usr/lib64/libtheoradec.so
+/usr/lib64/libogg.sovtkRenderingVolumeAMRvtkIONetCDFvtkIOPLYvtkIOParallelvtkTestingRenderingvtkIOVideovtkViewsContext2DvtkRenderingImagevtkRenderingLICvtkRenderingLODvtkImagingMathvtkImagingMorphologicalvtkLocalExamplevtkImagingStatisticsvtkImagingStencilvtkViewsGeovisvtkRenderingQt/usr/lib64/libpython2.7.sovtkRenderingVolumeOpenGLvtkWrappingJavavtkWrappingToolsvtkWrappingPythonCore
+
 '''
