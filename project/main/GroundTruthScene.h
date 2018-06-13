40d20b2bc432e3ceb15d32b96896ce2197950ca3
Vikas <veikas@mangalyaan>
2018-06-13 17:24:14 +0200

    sensor_count based on max_allowed_sensor_groups

--- project/main/GroundTruthScene.h
+++ project/main/GroundTruthScene.h
@@ -216,20 +216,6 @@ private:
                     "   <Debug enable=\"false\" detection=\"false\" road=\"false\" position=\"false\" dimensions=\"false\" camera=\"false\" packages=\"false\" culling=\"false\" /> "
                     "</Sensor>";
 
-    std::string module_manager_libModuleSensor_PerfectInertialTemplate_right =
-            "<Sensor name=\"Sensor_MM\" type=\"video\" > "
-                    "   <Load lib=\"libModuleCameraSensor.so\" path=\"/local/git/MotionFlowPriorityGraphSensors/VIRES/VTD.2.1/Data/Projects/../Distros/Distro/Plugins/ModuleManager\" /> "
-                    "   <Player name=\"MovingCar\"/> "
-                    "   <Frustum near=\"1.000000\" far=\"40.000000\" left=\"30.000000\" right=\"30.000000\" bottom=\"20.000000\" top=\"20.000000\" /> "
-                    "   <Position dx=\"2.000000\" dy=\"1.000000\" dz=\"1.500000\" dhDeg=\"0.000000\" dpDeg=\"0.000000\" drDeg=\"0.000000\" /> "
-                    "   <Origin type=\"usk\" /> "
-                    "   <Cull maxObjects=\"10\" enable=\"true\" /> "
-                    "   <Port name=\"RDBout\" number=\"65535\" type=\"TCP\" sendEgo=\"false\" /> "
-                    "   <Filter objectType=\"none\" />"
-                    "   <Filter objectType=\"pedestrian\" /> "
-                    "   <Filter objectType=\"vehicle\" /> "
-                    "   <Debug enable=\"false\" detection=\"false\" road=\"false\" position=\"false\" dimensions=\"false\" camera=\"false\" packages=\"false\" culling=\"false\" /> "
-                    "</Sensor>";
 
     std::string module_manager_libModuleSingleRaySensor =
             "<Sensor name=\"simpleSensor\" type=\"radar\">\n"
